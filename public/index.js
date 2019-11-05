8d5b06adbe7ba96dc115b0765ed12ab3f9f67e1a
HuyN1349 <huy.nguyen1349@gmail.com>
2019-11-05 01:42:35 -0800

    Add animation on page scroll.

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ $(document).ready(function($) {
   var $window = $(window);
 
   function resize() {
-    if ($window.width() < 1000) {
+    if ($window.width() < 988) {
       $('.exp-container').removeClass('flex-row').addClass('flex-column');
       $('.exp-content').removeClass('flex-column').addClass('flex-row');
     } else {
